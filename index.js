afb69f645896adbc811c0acb119c4c9cbfd04b8c
Connor Kirkwood <ckirkwood7464@gmail.com>
2019-12-10 22:48:04 -0500

    Added additional get requests and post request

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express')
-const movies = require('./movies.json')
-
+const moviesData = require('./movies.json')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.get('/movies', (request, response) => {
-    response.send(movies)
+    response.send(moviesData)
 })
 
-app.get('/movies/:x', (request, response) => {
-    const matchingMovies = movies.filter((item) => {
-        return item.x === request.params.x
+app.get('/movies/:id', (request, response) => {
+    const matchingMovie = moviesData.find((movie) => {
+        return movie.id == request.params.id
+    })
+
+    if (matchingMovie) {
+        response.send(matchingMovie)
+    } else {
+        response.sendStatus(404)
+        
+    }
+})
+
+app.get('/directors/:id', (request, response) => {
+    const foundMovie = moviesData.find((movie) => {
+        return movie.id == request.params.id
+    })
+    const matchingMovies = foundMovie && moviesData.filter((movie) => {
+        return movie.directors === foundMovie.directors
+    })
+    if (matchingMovies.length) {
+        response.send(matchingMovies)
+    } else {
+        response.sendStatus(404)
+    }
+})
+
+app.get('/genres/:genre', (request, response) => {
+    const matchingMovies = moviesData.filter((movie) => {
+        return movie.genre == request.params.genre
     })
 
     if (matchingMovies.length) {
@@ -17,6 +44,21 @@ app.get('/movies/:x', (request, response) => {
     } else {
         response.sendStatus(404)
     }
+})
+
+app.use(bodyParser.json())
+
+app.post('/movies', bodyParser.json(), (request, response) => {
+    const {title, directors, releaseDate, rating, runtime, genre, id} = request.body
+
+    if (!title || !directors || !releaseDate || !rating || !runtime || !genre || !id) {
+        response.status(400).send('The following attributes are required: title, directors, releaseDate, rating, runtime, genre, and id')
+    }
+
+    const newMovie = {title, directors, releaseDate, rating, runtime, genre, id}
+
+    moviesData.push(newMovie)
+    response.status(201).send(newMovie)
 })
 
 app.all('*', (request, response) => {
